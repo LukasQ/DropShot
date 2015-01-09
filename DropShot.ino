52dbb4dc97259bcb05918395885ae3ecedf1a1a5
LukasQ <LukasQ@users.noreply.github.com>
2015-01-10 00:17:37 +0100

    Create DropShot.ino

---
+++ DropShot.ino
@@ -0,0 +1,76 @@
+#define LED 13 //LED Pin for Status
+#define FLASH 2 // Optocouppler PIN for the flash
+#define SHUTTER 4 // Optocouppler Pin for the shutter
+#define TRIGGER 0 // Lightbar receiver to start the rec
+#define VALVE 6 // Valve Output Pin -> Transistor
+#define SWITCH 8 // Trigger button to open the valve
+
+#define DROPSIZE 200 // Valve open time
+#define FLASHDELAY 201 // Delay between trigger and flash
+//Usually t= sqrt((2h)/9,81)
+
+int value = 0;
+boolean done = false;
+
+void setup() {
+  //Pin modes
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, HIGH); //Show status
+  pinMode(FLASH, OUTPUT);
+  pinMode(SHUTTER, OUTPUT);
+  pinMode(VALVE, OUTPUT);
+  pinMode(TRIGGER, INPUT);
+  pinMode(SWITCH, INPUT);
+
+  Serial.begin(9600);  //Begin serial communcation -> Debug
+
+  // collect sensor data
+  for(int i = 0; i <= 10; i++){ 
+    value += analogRead(TRIGGER);
+    delay(100);
+  }
+  value = value/10; // average sensor data
+  digitalWrite(LED, LOW); //End startup
+}
+
+void loop() {
+  //Debug: Serial.println(analogRead(TRIGGER)); //Write the value of the photoresistor to the serial monitor.
+  
+  //Start the shooting
+  if(digitalRead(SWITCH) && done == false){
+    valve();
+    Serial.print("value opende");
+    done=true;
+  }
+
+// If trigger -> make a photo
+  if(analogRead(TRIGGER) > value){
+    shoot();
+    delay(FLASHDELAY);
+    flash();
+  }
+}
+
+//Photo trigger function
+void shoot(){
+  digitalWrite(SHUTTER, HIGH);
+  delay(60);
+  digitalWrite(SHUTTER, LOW);
+  done = true;
+}
+
+//Flash light function
+void flash(){
+  digitalWrite(FLASH, HIGH);
+  delay(60);
+  digitalWrite(FLASH, LOW);
+  done = true;
+}
+
+//Valve open function
+void valve(){
+  digitalWrite(VALVE, HIGH);
+  delay(DROPSIZE);
+  digitalWrite(VALVE, LOW);
+  done = true;
+}
